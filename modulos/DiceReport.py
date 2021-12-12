4d127c4467e69d68d20faa3ac47a8dfb152b936f
Mateus de Almeida <contato.mat55@gmail.com>
2021-12-12 00:36:10 -0300

    at

--- modulos/DiceReport.py
+++ modulos/DiceReport.py
@@ -20,13 +20,3 @@ class DiReport:
         self.button, self.values = report.Read()
     def Iniciar(self):
         print("namteo")
-##-------------------------------------------#-------------------------------------------------##
-##-------------------------------------------#-------------------------------------------------##      
-while True:
-    window, event, values = sg.read_all_windows()
-    if window == Tela_Inicial and event == sg.WIN_CLOSED:
-        break
-    if window == Tela_Inicial and event == 'Salvar':
-        Tela_Dados = Report() #chamar a def não a class
-        Tela_Inicial.hide()
-        
