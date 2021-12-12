6e760deefa5064e8942554cd9bcaf8b6bd2e9ec5
Mateus de Almeida <contato.mat55@gmail.com>
2021-12-12 00:10:02 -0300

    v04
    modificação de while

--- main.py
+++ main.py
@@ -8,10 +8,3 @@ Tela_Inicial = Header.WindowHeader()
 Tela_Dados = DiceReport.DiReport()
 Tela_Dados.Iniciar()
 Tela_Inicial.Iniciar()
-while True:
-    window, event, values = sg.read_all_windows()
-    if window == Tela_Inicial and event == sg.WIN_CLOSED:
-        break
-    if window == Tela_Inicial and event == 'Salvar':
-        Tela_Dados = Report() #chamar a def não a class
-        Tela_Inicial.hide()
