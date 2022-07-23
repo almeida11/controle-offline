637d10368a007db8b6baae74260bc8fa55ffc3ab
Mateus Almeida <mateus.almeidaif@gmail.com>
2022-07-23 10:52:56 -0300

    Refac
    Modificado as seleções de turma e turno de check box para lista de seleção, para reduzir a quantidade de condições para atribuir um valor a variável.

--- main.py
+++ main.py
@@ -4,22 +4,17 @@ def make_WindowTurn():
         layout = [
         #CABEÇALHO
         #seleção de turno
-        [sg.Text("Informações do Turno")],
-        [sg.Checkbox("00:00 às 08:00",key = '00')],
-        [sg.Checkbox("08:00 às 16:00",key = '01')],
-        [sg.Checkbox("16:00 às 00:00",key = '02')],
+        [sg.Text('Turno')],
+        [sg.Combo(["00:00 as 08:00", "08:00 as 16:00", "16:00 as 00:00"], key="turno")],
         #data
-        [sg.Text('Data'), sg.Input(key='data')],
+        [sg.Text('Data')],
+        [sg.Input(key='data')],
         #seleção de turma
-        [sg.Checkbox('Turma A',key = 'var_A'),sg.Checkbox('Turma B',key = 'var_B')],  
-        [sg.Checkbox('Turma C',key = 'var_C'),sg.Checkbox('Turma D',key = 'var_D')],
+        [sg.Text('Turma')],
+        [sg.Combo(["Turma A", "Turma B", "Turma C", "Turma D"], key="turma")],
         #inspetor
         [sg.Text('Inspetor')],
-        [sg.Combo(["Rayson", "Gabriel", "Vinnicius", "Alan"], key="inspetor")],
-        #produto
-        [sg.Text("Produto"), sg.Input(key='produto')],
-        #bitola
-        [sg.Text("Bitola"), sg.Input(key='bitola')],
+        [sg.Combo(["Fernando", "Josiel Junior", "Marcos Vinicius", "Alex"], key="inspetor")],
         #gerar cabeçalho
         [sg.Button('Salvar'), sg.Button('Fechar')],
     ]
@@ -47,43 +42,14 @@ while True:
         break
     if window == WindowTurn:
 #salvando informações e escrevendo no txt
-        turno0 = values['00']
-        turno1 = values['01']
-        turno2 = values['02']
-        turno_real = ''
+        turno = values['turno']
         #PUXANDO DADOS DATA
         data = values['data']
         #PUXANDO DADOS DE TURMA
-        var_1 = values['var_A']
-        var_2 = values['var_B']
-        var_3 = values['var_C']
-        var_4 = values['var_D']
-        turma_real = ''
+        turma = values['turma']
         #PUXANDO DADOS INSPETOR
         inspetor = values['inspetor']
         #PUXANDO DADOS PRODUTO
-        produto = values['produto']
-        #PUXANDO DADOS BITOLA
-        bitola = values['bitola']
-        if turno0 == 1:
-            turno_real = '00:00 as 08:00'
-        elif turno1 == 1:
-            turno_real = '08:00 as 16:00'
-        elif turno2 == 1:
-            turno_real = "16:00 as 00:00"
-        else:
-            turno_real = "Turno nao selecionado!"
-##-------------------------------------------#-------------------------------------------------##
-        if var_1 == 1:
-            turma_real = "A"
-        elif var_2 == 1:
-            turma_real = "B"
-        elif var_3 == 1:
-            turma_real = "C"
-        elif var_4 == 1:
-            turma_real = "D"
-        else:
-            turma_real = "Turma nao selecionada!"
         if event == 'Salvar':
             WindowTurn.hide()
             WindowDices = make_WindowDices()
@@ -93,9 +59,7 @@ while True:
             "*Data:* {}""\n"
             "*Turma:* {}""\n"
             "*Inspetor:* {}""\n"
-            "*Produto:* {}""\n"
-            "*Bitola:* {}""\n"
-            "*Rendimento Metalico Total do Turno:* ""\n".format(turno_real, data, turma_real, inspetor, produto, bitola))
+            "*Rendimento Metalico Total do Turno:* ""\n".format(turno, data, turma, inspetor))
     if window == WindowDices and event in (sg.WIN_CLOSED, 'Fechar'):
         break
     if window == WindowDices:
